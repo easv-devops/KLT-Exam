ee5ca6f9dfc5715858fa9bda98925a2eb4f09814
hoeeg79 <93433669+hoeeg79@users.noreply.github.com>
2024-04-22 21:00:51 +0200

    first changes
    Where to begin. Added the deploy to test pipeline. watchtower will fail on first build, but want the images published to docker hub, before launching on server.
    
    Included is also more secrets, another gitignored file for watchtower token

--- backend/infrastructure/Utilities.cs
+++ backend/infrastructure/Utilities.cs
@@ -4,5 +4,5 @@ public class Utilities
 {
     private static readonly string DBPassword = new(Environment.GetEnvironmentVariable("PGPASSWORD"));
     public static readonly string connectionStringDev = "Server=localhost:5432;Database=postgres;User Id=postgres;Password=" + DBPassword + ";";
-    public static readonly string connectionStringProd = "Server=app-database:5432;Database=postgres;User Id=postgres;Password=" + DBPassword + ";";
+    public static readonly string connectionStringProd = "Server=database:5432;Database=postgres;User Id=postgres;Password=" + DBPassword + ";";
 }
