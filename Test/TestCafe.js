74662fb8107f18dce962bb5f9338938e9e9bdd5c
Tomas Müller <tomas@muellers.dk>
2024-04-29 09:30:33 +0200

    Update testcafe

---
+++ Test/TestCafe.js
@@ -0,0 +1,37 @@
+const {Selector} = require("testcafe");
+
+//fixture('Getting Started').page('http://144.91.64.53:8081/');
+fixture('Getting Started').page('http://144.91.64.53:8081/');
+
+test('Testing of testCafe', async  t =>
+{
+  //Constanter til dropdown 1
+
+  const H1 = Selector('#h1-1') //Her vælges h1
+
+    await t
+
+    //input
+      .typeText('#input1','Jens Jensen')
+      .typeText('#input2','Jensen vej 1')
+      .typeText('#input3','1000')
+      .typeText('#input4','Jensby')
+
+
+
+      //Click
+      .click('#button1')
+      .wait(2000)
+
+
+      .expect(H1.textContent).eql('Jens Jensen') //Her testes om værdien i paragraf er Tekst1
+
+});
+
+
+
+//kør testen
+//testcafe chrome TestCafe.js --live
+//Ctrl+S stopper testen
+//Ctrl+r genstarter testen
+
