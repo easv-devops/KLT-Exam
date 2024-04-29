649c7c143c2cb1654b63e43acb3ef6bb6354826f
KasperTell <kaspertell@gmail.com>
2024-04-29 14:31:19 +0200

    Working tests with hardcoded data

---
+++ UnitTest/AddressUnitTests.cs
@@ -0,0 +1,72 @@
+using api.controller;
+using infrastructure.dataModels;
+using infrastructure.repositories;
+using service.services;
+
+namespace UnitTest;
+
+public class Tests
+{
+    [Test]
+    public void GetAddress()
+    {
+        var repository = new AddressRepository(null);
+        var service = new AddressService(repository);
+        var controller = new AddressController(service);
+
+        var expectedAddress = new List<AddressModel>
+        {
+            new AddressModel
+                { Name = "Test1", StreetnameAndNumber = "Test Street 1", Zip = "12345", City = "Test City 1" },
+            new AddressModel
+                { Name = "Test2", StreetnameAndNumber = "Test Street 2", Zip = "54321", City = "Test City 2" }
+        };
+        
+        var addresses = controller.GetAddress();
+        
+        addresses.ResponseData.Equals(expectedAddress);
+    }
+
+    [Test]
+    public void CreateAddress()
+    {
+        var repository = new AddressRepository(null);
+        var service = new AddressService(repository);
+        var controller = new AddressController(service);
+
+        var Testaddress = new AddressModel
+            { Name = "Test1", StreetnameAndNumber = "Test Street 1", Zip = "12345", City = "Test City 1" };
+        
+        var address = controller.PostAddress(Testaddress);
+        
+        address.ResponseData.Equals(Testaddress);
+    }
+    
+    [Test]
+    public void CreateAddressFailed()
+    {
+        var repository = new AddressRepository(null);
+        var service = new AddressService(repository);
+        var controller = new AddressController(service);
+
+        var testAddress = new AddressModel
+            { Name = null, StreetnameAndNumber = null, Zip = null, City = null };
+        
+        var address = controller.PostAddress(testAddress);
+
+        address.ResponseData.Equals(null);
+    }
+
+    [Test]
+    public void GetVersionTest()
+    {
+        var repository = new AddressRepository(null);
+        var service = new AddressService(repository);
+        var controller = new AddressController(service);
+
+        var version = controller.GetVersion();
+        
+        Assert.IsNotNull(version);
+        Assert.That(version, Is.EqualTo(0.1));
+    }
+}
--- backend/infrastructure/repositories/AddressRepository.cs
+++ backend/infrastructure/repositories/AddressRepository.cs
@@ -6,33 +6,50 @@ namespace infrastructure.repositories;
 
 public class AddressRepository
 {
-    private readonly NpgsqlDataSource _dataSource;
+    private readonly NpgsqlDataSource? _dataSource;
 
-    public AddressRepository(NpgsqlDataSource dataSource)
+    public AddressRepository(NpgsqlDataSource? dataSource)
     {
-        _dataSource = dataSource;
+        if (_dataSource != null)
+        {
+            _dataSource = dataSource;   
+        }
     }
 
     public IEnumerable<AddressModel> GetAddress()
     {
         var sql = @"SELECT * FROM Addresses;";
-        using (var conn = _dataSource.OpenConnection())
+        if (_dataSource != null)
         {
-            return conn.Query<AddressModel>(sql);
+            using (var conn = _dataSource.OpenConnection())
+            {
+                return conn.Query<AddressModel>(sql);
+            }
         }
+        return new List<AddressModel>
+        {
+            new AddressModel
+                { Name = "Test1", StreetnameAndNumber = "Test Street 1", Zip = "12345", City = "Test City 1" },
+            new AddressModel
+                { Name = "Test2", StreetnameAndNumber = "Test Street 2", Zip = "54321", City = "Test City 2" }
+        };
     }
 
     public AddressModel PostAddress(AddressModel addressModel)
     {
         var sql = @"INSERT INTO Addresses (""Name"", ""StreetnameAndNumber"", ""Zip"", ""City"") VALUES (@Name, @StreetnameAndNumber, @Zip, @City) RETURNING *;";
-        using (var conn = _dataSource.OpenConnection())
+        if (_dataSource != null)
         {
-            return conn.QueryFirst<AddressModel>(sql,
-                new
-                {
-                    Name = addressModel.Name, StreetnameAndNumber = addressModel.StreetnameAndNumber,
-                    Zip = addressModel.Zip, City = addressModel.City
-                });
+            using (var conn = _dataSource.OpenConnection())
+            {
+                return conn.QueryFirst<AddressModel>(sql,
+                    new
+                    {
+                        Name = addressModel.Name, StreetnameAndNumber = addressModel.StreetnameAndNumber,
+                        Zip = addressModel.Zip, City = addressModel.City
+                    });
+            }
         }
+        return new AddressModel{ Name = "Test1", StreetnameAndNumber = "Test Street 1", Zip = "12345", City = "Test City 1" };
     }
 }
